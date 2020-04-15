abc34605219cf9ae44f746e00c634c8f8aaddb18
touchy <prashantgecg@gmail.com>
2020-04-15 17:43:56 +0530

    context with local storage react 2020

---
+++ build/precache-manifest.4f00ccaff65fe3c215f8ceddd591f0db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a33066cd64e3b935d86b044682429b63",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82bbef5e4f52814741c3",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "c0a37630d51e4d8f2dd4",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "82bbef5e4f52814741c3",
+    "url": "/static/js/2.a7537194.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a7537194.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a37630d51e4d8f2dd4",
+    "url": "/static/js/main.b0660b7b.chunk.js"
+  },
+  {
+    "revision": "9b9b1853cf6aa4445599",
+    "url": "/static/js/runtime-main.0dd6ad9f.js"
+  }
+]);
